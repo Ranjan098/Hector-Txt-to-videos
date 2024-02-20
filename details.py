87c5441eff8b82fe95646ac0f3760b2de267a748
hectorog6612 <159784158+hectorog6612@users.noreply.github.com>
2024-02-20 16:58:30 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id = 29410389
-api_hash = "0c716764715886f6641477ffbb63e1ee"
-bot_token = "5946934925:AAGoALZ3-Cr8hQaBdzHhmW3pknt-h1LVVpI"
-sudo_groups = -1001777454744
+api_id = 10608382
+api_hash = "ea04136e7acb517f48ccf8425ca99243"
+bot_token = "7109083819:AAGKSPm8tc7ef-wPjU2oriK3W95tBQNUSRE"
+sudo_groups = -444557034
 
