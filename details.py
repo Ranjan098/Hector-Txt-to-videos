0da302ee966d4cb2ea383d4b1fba30988a54564c
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2024-03-25 03:06:38 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id = 10608382
-api_hash = "ea04136e7acb517f48ccf8425ca99243"
-bot_token = "7109083819:AAGKSPm8tc7ef-wPjU2oriK3W95tBQNUSRE"
-sudo_groups = -444557034
+api_id = 12606917
+api_hash = "f25113b8c17dca6fa7abda53a86bd4f7"
+bot_token = "6740676504:AAG0VvpBW1kj0h5eaBoZgM4SkmqPnqtqE4I"
+sudo_groups = -1001699711548
 
